83fc2efea8b7e8ea5b0f8737a4d442a07aafcab7
jpatterson-cYRYJA <jpatterson@instructors.2u.com>
2023-06-01 20:34:34 -0700

    added comments ot script.js in form directory

--- form/script.js
+++ form/script.js
@@ -10,45 +10,49 @@ console.log(formElement, nameElement, emailElement, confirmEmailElement)
 
 function onFormSubmit(event) {
 
-    event.preventDefault();
+    event.preventDefault(); // prevent the page from reloading when the script is sent in
     console.log(nameElement.val());
-
+    // simple if statments to compare if the emails that are entered are the same
     if(emailElement.val().toLowerCase() !== confirmEmailElement.val().toLowerCase()){
         alert("your emails to not match!!");
         return;
     }
 
     // create a newUser object
-
     var newUser = {
-        name: nameElement.val().toLowerCase(),
+        name: nameElement.val().toLowerCase(), // key: value pairs with the key being whatever we want and the value being user input
         email: emailElement.val().toLowerCase()
     }
 
-    var users = JSON.parse(localStorage.getItem('users'));
+    var users = JSON.parse(localStorage.getItem('users')); // grab users from local storage if they exist, else it will return null
     console.log(users)
+    // if statement for what to do with the users from local storage
     if (users === null) {
-        // push new user into empty users array
+        // push newUser into empty users array
         allUsers.push(newUser);
-        localStorage.setItem('users', JSON.stringify(allUsers))
+        localStorage.setItem('users', JSON.stringify(allUsers)) // save the updated users array to local storage
+
+        // if an array of users exists
     } else if (users.length > 0) {
         // setting the empty users array to the users that exist in local storage
         allUsers = users;
-        allUsers.push(newUser)
-        localStorage.setItem('users', JSON.stringify(allUsers))
+        allUsers.push(newUser) // then add the new users
+        localStorage.setItem('users', JSON.stringify(allUsers)) // save allUsers to local storage and overwrite previous array
     }
 }
 
+// simple on submit for our onFormSubmit() function
 formElement.on('submit', onFormSubmit);
 
+// function for doing a fetch request
 function displayCatFactPlease() {
 
     fetch('https://catfact.ninja/fact')
-        .then(res => res.json()) // 
+        .then(res => res.json()) // res.json() 
         .then(data => {
             // Handle the retrieved data
             console.log(data);
-
+            // append the fact to the textContent of an html element with an id of #display-cat-fact using jquery
             $("#display-cat-fact").text(data.fact);
 
         })
@@ -59,18 +63,18 @@ function displayCatFactPlease() {
 
 }
 
-
+// call our onclick for our cat-fact-button
 $("#cat-fact-button").on("click", displayCatFactPlease)
 
-
+// set up a request url
 var requestUrl = "https://catfact.ninja/fact";
-
+// the same request as above except as an ajax request and only happens on page reload
 $.ajax({
-    url: requestUrl,
-    method: 'GET'
+    url: requestUrl, // plug in our requestUrl
+    method: 'GET' // specify which method to use
 }).then(function (res) {
-    console.log(res)
-    $("#display-cat-fact").text(res.fact);
+    console.log(res) // console.log for testing
+    $("#display-cat-fact").text(res.fact); // append to the page
 
 })
 
